94268c0320e7f0762b32214ae283415e7025c47a
Navneet Shahi <138277449+navneetshahi14@users.noreply.github.com>
2025-10-11 21:59:50 +0530

    Update app.ts

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,7 @@ app.use(express.json({limit:"50mb"}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin:['https://learning-management-system-5dmbcpyrc.vercel.app/'],
+    origin:['https://learning-management-system-ten-drab.vercel.app/'],
     credentials:true
 }))
 
