99ebe0f028203e82263a25ec977800b5e10fe2bb
navneetshahi14 <navneet.shahi2004@gmail.com>
2025-10-11 21:23:21 +0530

    updating cors

--- server/app.ts
+++ server/app.ts
@@ -19,7 +19,7 @@ app.use(express.json({limit:"50mb"}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://learning-management-system-5dmbcpyrc.vercel.app/'],
     credentials:true
 }))
 
